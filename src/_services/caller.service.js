426875074a1785147f3b94d4189a9eaa157a7d5f
Grossemain <grossemain@gmail.com>
2024-08-26 17:52:31 +0200

    1/ Installation de React 2/ Installation des dépendances et librairies
    • React-Router-Dom
    • Axios
    • Bootstrap et sass
    Librairie Mui material Ui pour le front
    
    3/ Création des dossiers de base du projet
    4/ Création du caller service pour l'instance axios

---
+++ src/_services/caller.service.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+const Axios = axios.create({
+  baseURL: "http://127.0.0.1:8000",
+  headers: { "Content-Type": "multipart/form-data" },
+});
+
+// Intercepteur pour la mise en place du token dans la requête
+Axios.interceptors.request.use((request) => {
+  if (accountService.isLogged()) {
+    request.headers.Authorization = "Bearer " + accountService.getToken();
+  }
+  return request;
+});
+
+// Intercepteur de réponse API pour vérification de la session
+Axios.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response.status === 401) {
+      accountService.logout();
+      window.location = "/auth/login";
+    } else {
+      return Promise.reject(error);
+    }
+  }
+);
+
+export default Axios;
---
+++ src/_utils/error.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Error = () => {
+    return (
+        <div>
+            Error 404 !
+            La page que vous demandez n'existe pas.
+            <a href="/">Retour à la page d'accueil</a>
+        </div>
+    );
+};
+
+export default Error;
