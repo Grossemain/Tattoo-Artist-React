bb894c068f9669500bfa9dae7f482de9f6dab2bd
Grossemain <grossemain@gmail.com>
2024-09-03 19:49:08 +0200

    Modification des Card avec Scss

--- src/components/public/Users/UserCard.js
+++ src/components/public/Users/UserCard.js
@@ -1,21 +1,43 @@
-import React from 'react';
-import {Card, Button} from 'react-bootstrap/';
-import { Link } from 'react-router-dom'
-
+import React from "react";
+import { Card, Button, CardGroup, Row, Col } from "react-bootstrap/";
+import { Link } from "react-router-dom";
 
 const UserCard = ({ user }) => {
-    return (
-        <Card className="md-6">
-                      <Card.Body>
-                        <Card.Title><h2>{user.pseudo_user}</h2></Card.Title> 
-                        <Card.Text><em>{user.description	}</em></Card.Text>
-                        <Card.Text><span className='bg-warning rounded-pill p-2'>{user.email_contact}</span></Card.Text>
-                        <Button variant="primary">
-                            <Link className="text-light text-decoration-none"
-                            to={`/tatoueur/${user.user_id}`}>{user.pseudo_user}</Link>
-                        </Button>
-                      </Card.Body>
-                    </Card>
-    );
+  return (
+    <CardGroup>
+      <Card className="card md-6">
+        <Card.Img
+          className="card-image"
+          variant="top"
+          src={`http://127.0.0.1:8000/storage/uploads/${user.img_profil}`}
+          alt={`${user.pseudo_user}-${user.city}`}
+        />
+        <Card.Title className="card-title">
+          <h2>{user.pseudo_user}</h2>
+        </Card.Title>
+        <Card.Body className="card-content">
+          <Row>
+            <Col className="colonne">
+              <span className="cell">{user.city}</span>
+            </Col>
+            <Col className="colonne">
+              <span className="cell">{user.departement}</span>
+            </Col>
+            <Col className="colonne">
+              <span className="cell">{user.departement}</span>
+            </Col>
+          </Row>
+        </Card.Body>
+        <Row className="card-button">
+          <Link
+            className="text-light text-decoration-none"
+            to={`/tatoueur/${user.user_id}`}
+          >
+            {user.pseudo_user}
+          </Link>
+        </Row>
+      </Card>
+    </CardGroup>
+  );
 };
 export default UserCard;
