717513d6e70f73bd72b88e1a29a28c2dc6f922ff
Grossemain <grossemain@gmail.com>
2024-09-16 17:02:37 +0200

    Ajout du boutton de deconnection dans le header

--- src/components/public/Header.js
+++ src/components/public/Header.js
@@ -2,22 +2,29 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import LogoutButton from "../../components/admin/LogoutButton";
 
 import Logo from "../../components/public/Logo";
+import { accountService } from "../../_services";
 
 const Header = () => {
+  const userRole = accountService.getRole(); // Fonction pour obtenir le rôle de l'utilisateur
+  console.log("role_id:", userRole);
   return (
     <div className="navMenu">
       <Navbar className="justify-content-end border" bg="light" expand="lg">
         <Nav>
           <Nav.Link href="/register/">Vous etes tatoueur?</Nav.Link>
           <Nav.Link href="/auth/login/">Connexion</Nav.Link>
+          {userRole == 1 && <Nav.Link href="/mon-compte/">Mon compte</Nav.Link>}
+          {userRole == 2 && <Nav.Link href="/admin/">Admin</Nav.Link>}
         </Nav>
+        {userRole ==1 || userRole ==2 && <LogoutButton />}
       </Navbar>
       <Navbar bg="light" expand="lg">
         <Container fluid>
-        <Logo />
-        <Navbar.Toggle aria-controls="navbarScroll" />
+          <Logo />
+          <Navbar.Toggle aria-controls="navbarScroll" />
           <Navbar.Collapse id="navbarScroll">
             <Nav>
               <Nav.Link href="/tatoueurs">Tatoueurs</Nav.Link>
@@ -27,7 +34,7 @@ const Header = () => {
               <Nav.Link href="/tattooshops">Salons</Nav.Link>
               <Nav.Link href="/articles">Blog</Nav.Link>
             </Nav>
-            </Navbar.Collapse>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
     </div>
